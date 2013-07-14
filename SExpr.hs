a9885fc5f5509ca05ce359859dc83da3273c6850
Dan Feltey <dfeltey@gmail.com>
2013-07-13 18:18:25 -0800

    branch with short-circuiting and/or operators added

--- SExpr.hs
+++ SExpr.hs
@@ -20,7 +20,10 @@ desugar sexp = case sexp of
     BoolS b -> BoolE b
     AppS s1 s2 -> App (desugar s1) (desugar s2)
     LambdaS x s -> Lambda x (desugar s)
-    BinopS op s1 s2 -> Binop op (desugar s1) (desugar s2)
+    BinopS op s1 s2 -> case op of
+    	   OR -> App (Lambda "$z" (IF (Var "$z") (Var "$z") (desugar s2))) (desugar s1)
+	   AND -> App (Lambda "$z" (IF (Var "$z") (desugar s2) (Var "$z") )) (desugar s1)
+	   _ -> Binop op (desugar s1) (desugar s2)
     IFS b s1 s2 -> IF (desugar b) (desugar s1) (desugar s2)
     LetS (x,s1) s2 -> App (Lambda x (desugar s2)) (desugar s1)
     PrintS e -> PrintE (desugar e)
